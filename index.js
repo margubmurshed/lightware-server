720649825fc27caab33b1cfa9ce0b9754866f354
Umme Salma Papon <papon.fsp@gmail.com>
2023-10-19 16:54:59 +0600

    payment intent api has been developed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.port || 5000;
 
 // middleware
@@ -16,98 +17,129 @@ app.use(express.json());
 const verifyJWt = (req, res, next) => {
   const authorization = req.headers.authorization;
 
-  if(!authorization) {
-    return res.status(401).send( {error: true, message: 'unauthorized access'} );
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
   }
 
   const token = authorization.split(' ')[1]; // ['bearer', token......]
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if(err){
-      return res.status(401).send( {error: true, message: 'unauthorized access'} );
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
 
     req.decoded = decoded;
     next();
-  }) 
+  })
 }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrq6pyr.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      const db = client.db('LightwareDB');
-      const productsCollection = db.collection('products');
-      app.get('/products', async(req, res) => {
-        const filter = req?.query?.id ? {_id: new ObjectId(req.query.id)} : {};
-        const result = await productsCollection.find(filter).toArray();
-        res.send(result);
-      })
-
-      app.get('/orders', verifyJWt, async(req, res) => {
-        const decoded = req.decoded;
-
-        if(decoded.email !== req.query?.email) {
-          return res.status(403).send( {error: true, message: 'forbidden access'} );
-        }
-
-        let query = {}
-        if(req.query?.email){
-          query = {email: req.query.email}
-        }
-
-        // const result = await [mongodb orders fetch instruction by passing the query object]
-        // res.send(result)
-
-        res.send( {error: false, message: 'working'} ); // testing message
-      })
-
-      app.post('/products', async(req, res) => {
-        const product = req.body || {};
-        const result = await productsCollection.insertOne(product);
-        res.send(result);
-      })
-      // jwt
-      app.post('/jwt', (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '12h'});
-        res.send({token});
-      })
-      app.patch('/products/:id', async(req, res) => {
-        const id = req.query.id;
-        const product = req.body || {};
-        const result = await productsCollection.updateOne({_id: new ObjectId(id)}, {$set: product});
-        res.send(result);
-      })
-
-      app.delete('/products/:id', async(req, res) => {
-        const id = req.params.id;
-        const result = await productsCollection.deleteOne({_id: new ObjectId(id)});
-        res.send(result);
-      })
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  run().catch(console.dir);
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db('LightwareDB');
+    const productsCollection = db.collection('products');
+    app.get('/products', async (req, res) => {
+      const filter = req?.query?.id ? { _id: new ObjectId(req.query.id) } : {};
+      const result = await productsCollection.find(filter).toArray();
+      res.send(result);
+    })
+
+    app.get('/orders', verifyJWt, async (req, res) => {
+      const decoded = req.decoded;
+
+      if (decoded.email !== req.query?.email) {
+        return res.status(403).send({ error: true, message: 'forbidden access' });
+      }
+
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+
+      // const result = await [mongodb orders fetch instruction by passing the query object]
+      // res.send(result)
+
+      res.send({ error: false, message: 'working' }); // testing message
+    })
+
+    app.post('/products', async (req, res) => {
+      const product = req.body || {};
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    })
+    // jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' });
+      res.send({ token });
+    })
+    app.patch('/products/:id', async (req, res) => {
+      const id = req.query.id;
+      const product = req.body || {};
+      const result = await productsCollection.updateOne({ _id: new ObjectId(id) }, { $set: product });
+      res.send(result);
+    })
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await productsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send(result);
+    })
+
+    /*-------------------------------------------
+        * APIs related to payment
+    --------------------------------------------*/ 
+
+
+
+    /*-------------------------------------------
+        * when client intends to make a payment
+          but payment is not charged yet. 
+    --------------------------------------------*/ 
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      console.log(price)
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send("Lightware Server is running");
+  res.send("Lightware Server is running");
 })
 
 app.listen(port, () => {
-    console.log(`Lightware Server is Running on port: ${port}`);
+  console.log(`Lightware Server is Running on port: ${port}`);
 })
